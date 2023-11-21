d20261b31cef0be21a1687fd0a7906eb75ec5966
Saadmahmud-CSE <mdsaad2038@gmail.com>
2023-11-21 19:12:12 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,105 @@
+function gradeCalc(grade, unit) {
+    if (grade === "A+") {
+      return 4.00 * unit;
+    } else if (grade === "A") {
+      return 3.75 * unit;
+    } else if (grade === "A-") {
+      return 3.50 * unit;
+    } else if (grade === "B+") {
+      return 3.25 * unit;
+    } else if (grade === "B") {
+      return 3.00 * unit;
+    } else if (grade === "B-") {
+      return 2.75 * unit;
+    }else if (grade === "C+") {
+      return 2.50 * unit;
+    }else if (grade === "C") {
+      return 2.25 * unit;
+    }else if (grade === "D") {
+      return 2.00 * unit;
+    }else if (grade === "F") {
+      return 0.00 * unit;
+    }
+  }
+  
+  let counter = 1;
+  
+  function addCourse() {
+    let addNew = document.createElement("form");
+    addNew.classList.add("add_new", `key-${counter}`);
+    const course_name = `
+    <form class="add_new key-${counter}">
+      <div><input type="text" placeholder="Course Code" class="courses key-${counter}" required></div>
+      <div><input type="number" placeholder="Credit Unit" class="credit-units key-${counter}" required></div>
+      <div><select class="grade key-${counter}" required>
+        <option value="select">Select</option>
+        <option value="4.00">A+</option>
+        <option value="3.75">A</option>
+        <option value="3.50">A-</option>
+        <option value="3.25">B+</option>
+        <option value="3.00">B</option>
+        <option value="2.75">B-</option>
+        <option value="2.50">C+</option>
+        <option value="2.25">C</option>
+        <option value="2.00">D</option>
+        <option value="0.00">F</option>
+      </select></div> 
+    </form>
+    `;
+    addNew.innerHTML = course_name;
+    document.getElementById("course-wrapper").appendChild(addNew);
+    counter++;
+  }
+  
+  function removeCourse() {
+    let mainForm = document.querySelector("form.add_new");
+    mainForm?.remove();
+  }
+  
+  const reports = [];
+  
+  /**
+   * @description calculates cgpa
+   */
+  function calcCgpa() {
+    const CGPAPARAGRAPH = document.getElementById("cgpa-calc");
+    const GRADESSELECT = document.querySelectorAll("select.grade");
+    const UNIT = document.querySelectorAll("input.credit-units");
+  
+    const courseReport = {};
+  
+    const listOfGrades = [];
+    const listOfUnits = [];
+    let totalUnits = 0;
+  
+    GRADESSELECT.forEach((e) => {
+      let GRADES = e.options;
+      const selectedIndex = e.selectedIndex;
+      const selectedGrade = GRADES[selectedIndex];
+      const gradeValue = selectedGrade.text.toUpperCase();
+      listOfGrades.push(gradeValue);
+    });
+    console.log(listOfGrades);
+  
+    UNIT.forEach((e) => {
+      const unitValue = parseInt(e.value);
+      totalUnits += unitValue;
+      listOfUnits.push(unitValue);
+    });
+    console.log(listOfUnits);
+  
+    let totalEarnedUnits = 0;
+  
+    for (let i = 0; i < listOfUnits.length; i++) {
+      totalEarnedUnits += gradeCalc(listOfGrades[i], listOfUnits[i]);
+    }
+    const gpa = totalEarnedUnits / totalUnits;
+    
+    if (gpa >= 0){
+      CGPAPARAGRAPH.textContent = "Your CGPA is " + gpa.toFixed(2);   
+    } else {
+      CGPAPARAGRAPH.textContent = "Please Enter a Valid Grade and Credit Units.";    
+    }
+    
+  }
+  
